bbadf6770f070f06d8334ead061c7efd977a02e0
zhi-k <chongz.kang@gmail.com>
2022-06-01 16:44:57 +0800

    fix(tsconfig): added typeroots fix(server): make sure port is always a number

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-require("dotenv").config();
-
 import express, { Application } from "express";
 
 import { config } from "./config";
 
 const app: Application = express();
 
-app.listen(config.port, () => console.log(`[${config.env}] Server has started on port ${config.port}`));
+const port = parseInt(config.port);
+
+app.listen(port, () => console.log(`[${config.env}] Server has started on port ${port}`));
