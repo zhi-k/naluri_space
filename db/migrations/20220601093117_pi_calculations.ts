628e71a12aca03d05a6787b7f45c383e660ccaa5
zhi-k <chongz.kang@gmail.com>
2022-06-01 22:03:23 +0800

    fix(migration): make elapsed_time column not nullable

--- db/migrations/20220601093117_pi_calculations.ts
+++ db/migrations/20220601093117_pi_calculations.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex): Promise<void> {
 	return knex.schema.createTable(tableName, (table) => {
 		table.bigIncrements("id", { primaryKey: true });
 		table.text("calculated_value").notNullable();
-		table.bigint("elapsed_time").unsigned();
+		table.bigint("elapsed_time").unsigned().notNullable();
 		table.timestamps(true, true);
 	});
 }
