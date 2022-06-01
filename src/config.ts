6c4d17bdcec5561b559f29ce120a75c8aec5cd10
zhi-k <chongz.kang@gmail.com>
2022-06-01 16:39:12 +0800

    fix(config): use brackets to access process env chore: added dev script

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const config = Object.freeze({
-	env: process.env.NODE_ENV ?? "development",
-	port: process.env.PORT ?? 8080,
+	env: process.env['NODE_ENV'] ?? "development",
+	port: process.env['PORT'] ?? 8080,
 })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 import express, { Application } from "express";
 
 import { config } from "./config";
 
 const app: Application = express();
 
-app.listen(config.port, () => console.log(`Server has started on port ${config.port}`));
+app.listen(config.port, () => console.log(`[${config.env}] Server has started on port ${config.port}`));
