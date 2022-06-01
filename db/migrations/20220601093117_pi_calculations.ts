61014a322e8ab4301509fefb9ca6d74e61affc2f
zhi-k <chongz.kang@gmail.com>
2022-06-01 17:34:59 +0800

    feat(knex): added pi_calculations table migration and knex scripts

---
+++ db/migrations/20220601093117_pi_calculations.ts
@@ -0,0 +1,18 @@
+import type { Knex } from "knex";
+
+const tableName = "pi_calculations";
+
+export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable(tableName, (table) => {
+		table.bigIncrements("id", { primaryKey: true });
+		table.text("calculated_value").notNullable();
+		table.bigint("elapsed_time").unsigned();
+		table.timestamps(true, true);
+	});
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTable(tableName);
+}
+
