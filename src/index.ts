2d1211a25b8b6daac90f82ffbe1d6f5ba20f06ec
zhi-k <chongz.kang@gmail.com>
2022-06-01 22:48:23 +0800

    feat(index): added 404 and error handler and added while loop to calculate pi on server start

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,50 @@
-import express, { Application } from "express";
-import helmet from 'helmet';
-import morgan from 'morgan';
+import express, { Application, Request, Response, NextFunction } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 
 import { config } from "./config";
 import routes from "./routes";
+import calculatePi from "./calculatePi";
 
 const app: Application = express();
 
 const port = parseInt(config.port);
 
 app.use(helmet());
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 app.use("/", routes);
 
 app.get("/health", (_, res) => res.sendStatus(200));
 
+app.use("*", (_, res) => {
+	return res.status(404).json({
+		error: "Page Not Found",
+	});
+});
+
+// @ts-ignore
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	res.status(500);
+
+	console.error(err);
+
+	return res.json({
+		error: "Something went wrong",
+		...(config.env !== "production" && { debug: err }),
+	});
+});
+
+
 app.listen(port, () => console.log(`[${config.env}] Server has started on port ${port}`));
+
+// Exception handler
+
+
+async function main() {
+	while (true) {
+		await calculatePi();
+	}
+}
+
+main();
