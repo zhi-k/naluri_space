e990376f29b003b1f4b9ee4349c0ed2f408322a4
zhi-k <chongz.kang@gmail.com>
2022-06-01 22:16:43 +0800

    feat(circumference_controller): added getSun logic to temporary return json

--- src/controllers/circumference_controller.ts
+++ src/controllers/circumference_controller.ts
@@ -1,9 +1,29 @@
 import type { Request, Response } from "express";
 
+import db from "../db";
+import PiCalculations from "../models/piCalculations";
+import { SunCircumference } from "../lib/circumference";
+import Pi from "../lib/pi";
+
+const piCalculationsModel = new PiCalculations(db);
+
 export default {
 
-	getSun: function(req: Request, res: Response) {
+	getSun: async function(_: Request, res: Response) {
+		const row = await piCalculationsModel.getLatestRow();
 
+		if (!row) {
+			return res.json({
+				pi: "0",
+				circumference: "0",
+			});
+		}
+
+		const sunCircumference = new SunCircumference(Pi.convertToBigInt(row.calculated_value), parseInt(row.id!)).calculate()
+
+		return res.json({
+			pi: row.calculated_value,
+			circumference: sunCircumference,
+		});
 	},
-
 };
